8637fae450eeb36595c887d3202040f5679203ff
brieuc <brieucmailpro@gmail.com>
2019-01-31 19:44:27 +0100

    First commit

---
+++ resources/js/components/Auth/LoginComponent.vue
@@ -0,0 +1,63 @@
+<template>
+    <main class="login background-color-secondary">
+        <section>
+            <div class="login__content columns">
+                <div class="login__content__left column is-6"></div>
+                <div class="column is-6 background-color-primary">
+                    <div class="login__content__right">
+                        <form @submit.prevent="loginForm()">
+                            <div class="level">
+                                <h1 class="is-size-3 has-text-white">Sign In</h1>
+                            </div>
+                            <div class="level">
+                                <div class="field full-w">
+                                    <label for="username" class="label has-text-white">Username</label>
+                                    <div class="control">
+                                        <input id="username" v-model="username" name="username" class="input" type="text"
+                                               placeholder="Username">
+                                    </div>
+                                    <!--<p class="help is-success">This username is available</p>-->
+                                </div>
+                            </div>
+                            <div class="level">
+                                <div class="field full-w">
+                                    <label for="password" class="label has-text-white">Password</label>
+                                    <div class="control">
+                                        <input id="password" v-model="password" class="input" type="password" placeholder="Password">
+                                    </div>
+                                    <!--<p class="help is-danger">This email is invalid</p>-->
+                                </div>
+                            </div>
+                            <div class="level">
+                                <button class="button background-color-secondary has-text-white" type="submit">Connect</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
+
+<script>
+	export default {
+		name: 'Login',
+        data() {
+		    return {
+		        username: '',
+                password: ''
+            }
+        },
+        methods: {
+	        loginForm() {
+                axios.post('/login', this.data)
+                    .then((response) => {
+                    	console.log(response)
+                    })
+                    .catch((error) => {
+	                    console.log(error)
+                    })
+            }
+        }
+	}
+</script>
---
+++ resources/js/components/Common/HeaderComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    <header>
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item">
+                    <img src="images/twitter.png">
+                </a>
+
+                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="navbarBasicExample" class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item">
+                        Home
+                    </a>
+                </div>
+
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <a class="button background-color-primary has-text-white">
+                                Sign up
+                            </a>
+                            <a class="button is-light">
+                                Log in
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+    export default {
+    	name: 'Header',
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
